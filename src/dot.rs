383ff8a10724544ef634189f77afa002ad774c8d
Agustin Borgna <agustin.borgna@quantinuum.com>
2023-02-16 11:31:21 +0000

    Import dot edge style defs from upstream

--- src/dot.rs
+++ src/dot.rs
@@ -4,10 +4,16 @@ use crate::PortIndex;
 
 use super::graph::Graph;
 
+pub trait DotEdge {
+    fn style(&self) -> String {
+        "None".to_string()
+    }
+}
+
 pub fn dot_string<'a, N, P>(graph: &'a impl Graph<'a, N, P>) -> String
 where
     N: 'a + Display + Clone,
-    P: 'a + Display + Clone,
+    P: 'a + Display + Clone + DotEdge,
 {
     let mut s = String::new();
 
@@ -35,13 +41,14 @@ fn add_edge_str<'a, N, P>(
     dot_s: &mut String,
 ) where
     N: 'a + Display + Clone,
-    P: 'a + Display + Clone,
+    P: 'a + Display + Clone + DotEdge,
 {
     let from_node = graph.port_node(from).expect("missing node").index();
     let to_node = graph.port_node(to).expect("missing node").index();
     let from_offset = graph.port_offset(from).expect("missing offset");
     let to_offset = graph.port_offset(to).expect("missing offset");
 
-    let edge_s = format!("{from_node} -> {to_node} [label=\"({from_offset}, {to_offset})\"]\n");
+    let style = graph.port_weight(from).unwrap().style();
+    let edge_s = format!("{from_node} -> {to_node} [label=\"({from_offset}, {to_offset})\", style={style}]\n");
     dot_s.push_str(&edge_s[..])
 }

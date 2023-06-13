75b16e32fe6dea49b8e342f0b7d8ad972c37465a
Agustin Borgna <agustin.borgna@quantinuum.com>
2023-06-13 17:04:42 +0100

    Implement dot rendering on LinkView trait

--- src/dot.rs
+++ src/dot.rs
@@ -2,28 +2,98 @@
 
 use std::fmt::Display;
 
-use crate::{Direction, Hierarchy, LinkView, NodeIndex, PortGraph, PortIndex, PortView, Weights};
+use crate::{Direction, Hierarchy, LinkView, NodeIndex, PortIndex, Weights};
 
 /// Style of an edge in a dot graph. Defaults to "None".
-pub type DotEdgeStyle = Option<String>;
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum NodeStyle {
+    /// Ignore the node. No edges will be connected to it.
+    Hidden,
+    /// Draw a box with the label inside.
+    Box(String),
+}
 
-/// Encode `PortGraph` and `Hierarchy` in dot format.
+impl NodeStyle {
+    /// Box node with label inside.
+    pub fn new(label: impl ToString) -> Self {
+        Self::Box(label.to_string())
+    }
+}
+
+impl Default for NodeStyle {
+    fn default() -> Self {
+        Self::Box(String::new())
+    }
+}
+
+/// Style of an edge in a dot graph. Defaults to "None".
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum PortStyle {
+    /// Do not draw a label. Edges will be connected to the node.
+    Hidden,
+    /// Just the port label
+    Text(String),
+    /// Draw a box around the label
+    Box(String),
+}
+
+impl PortStyle {
+    /// Custom style
+    pub fn new(style: impl ToString) -> Self {
+        Self::Box(style.to_string())
+    }
+}
+
+impl Default for PortStyle {
+    fn default() -> Self {
+        Self::Box(String::new())
+    }
+}
+
+/// Style of an edge in a dot graph. Defaults to "None".
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
+pub enum EdgeStyle {
+    /// Normal line
+    #[default]
+    Solid,
+    /// Dotted line
+    Dotted,
+    /// Dashed line
+    Dashed,
+    /// Custom style
+    Custom(String),
+}
+
+impl EdgeStyle {
+    /// Get the style as a string
+    pub fn as_str(&self) -> &str {
+        match self {
+            Self::Solid => "",
+            Self::Dotted => "dotted",
+            Self::Dashed => "dashed",
+            Self::Custom(s) => s,
+        }
+    }
+}
+
+/// Encode portgraph and `Hierarchy` in dot format.
 ///
 /// Calls the `nodes` and `ports` functions to get the labels for the nodes and ports.
 /// The `ports` function also returns the style of the edge connected to a port.
 ///
 /// Hierarchy relationships are shown as a separate tree of parent-child relationships
-pub fn hier_graph_dot_string_with(
-    graph: &PortGraph,
+pub fn hier_graph_dot_string_with<G: LinkView>(
+    graph: &G,
     forest: &Hierarchy,
-    nodes: impl FnMut(NodeIndex) -> String,
-    ports: impl FnMut(PortIndex) -> (String, DotEdgeStyle),
+    nodes: impl FnMut(NodeIndex) -> NodeStyle,
+    ports: impl FnMut(PortIndex) -> PortStyle,
+    edges: impl FnMut(G::LinkEndpoint, G::LinkEndpoint) -> EdgeStyle,
 ) -> String {
     let mut dot = String::new();
 
     dot.push_str("digraph {\n");
 
-    node_and_edge_strings(graph, &mut dot, nodes, ports);
+    node_and_edge_strings(graph, &mut dot, nodes, ports, edges);
 
     let hier_node_id = |n: NodeIndex| format!("hier{}", n.index());
 
@@ -53,77 +123,90 @@ pub fn hier_graph_dot_string_with(
     dot
 }
 
-/// Encode a `PortGraph` and `Hierarchy` in dot format.
-pub fn hier_graph_dot_string(graph: &PortGraph, hierarchy: &Hierarchy) -> String {
+/// Encode a portgraph and `Hierarchy` in dot format.
+pub fn hier_graph_dot_string(graph: &impl LinkView, hierarchy: &Hierarchy) -> String {
     hier_graph_dot_string_with(
         graph,
         hierarchy,
-        |n| n.index().to_string(),
-        |_| ("".to_string(), None),
+        |n| NodeStyle::new(n.index()),
+        |_| PortStyle::new(""),
+        |_, _| EdgeStyle::Solid,
     )
 }
 
-/// Encode a `PortGraph` in dot format.
-pub fn dot_string(graph: &PortGraph) -> String {
-    dot_string_with(graph, |n| n.index().to_string(), |_| ("".to_string(), None))
+/// Encode a portgraph in dot format.
+pub fn dot_string(graph: &impl LinkView) -> String {
+    dot_string_with(
+        graph,
+        |n| NodeStyle::new(n.index()),
+        |_| PortStyle::new(""),
+        |_, _| EdgeStyle::Solid,
+    )
 }
 
-/// Encode a `PortGraph` in dot format.
+/// Encode a portgraph in dot format.
 ///
 /// Uses the `weights` to label the nodes and ports.
-pub fn dot_string_weighted<N, P>(graph: &PortGraph, weights: &Weights<N, P>) -> String
+pub fn dot_string_weighted<N, P>(graph: &impl LinkView, weights: &Weights<N, P>) -> String
 where
     N: Display + Clone,
     P: Display + Clone,
 {
     dot_string_with(
         graph,
-        |n| weights.nodes[n].to_string(),
-        |p| (weights.ports[p].to_string(), None),
+        |n| NodeStyle::new(&weights.nodes[n]),
+        |p| PortStyle::new(&weights.ports[p]),
+        |_, _| EdgeStyle::Solid,
     )
 }
 
-/// Encode a `PortGraph` in dot format.
+/// Encode a portgraph in dot format.
 ///
 /// Calls the `nodes` and `ports` functions to get the labels for the nodes and ports.
 /// The `ports` function also returns the style of the edge connected to a port.
-pub fn dot_string_with(
-    graph: &PortGraph,
-    nodes: impl FnMut(NodeIndex) -> String,
-    ports: impl FnMut(PortIndex) -> (String, DotEdgeStyle),
+pub fn dot_string_with<G: LinkView>(
+    graph: &G,
+    nodes: impl FnMut(NodeIndex) -> NodeStyle,
+    ports: impl FnMut(PortIndex) -> PortStyle,
+    edges: impl FnMut(G::LinkEndpoint, G::LinkEndpoint) -> EdgeStyle,
 ) -> String {
     let mut dot = String::new();
 
     dot.push_str("digraph {\n");
-    node_and_edge_strings(graph, &mut dot, nodes, ports);
+    node_and_edge_strings(graph, &mut dot, nodes, ports, edges);
     dot.push_str("}\n");
     dot
 }
 
 /// Append the node and edge data in dot format to a string
-fn node_and_edge_strings(
-    graph: &PortGraph,
+fn node_and_edge_strings<G: LinkView>(
+    graph: &G,
     dot_str: &mut String,
-    mut nodes: impl FnMut(NodeIndex) -> String,
-    mut ports: impl FnMut(PortIndex) -> (String, DotEdgeStyle),
+    mut nodes: impl FnMut(NodeIndex) -> NodeStyle,
+    mut ports: impl FnMut(PortIndex) -> PortStyle,
+    mut edges: impl FnMut(G::LinkEndpoint, G::LinkEndpoint) -> EdgeStyle,
 ) {
-    for n in graph.nodes_iter() {
+    for node in graph.nodes_iter() {
         // Format the node as a table
+        let node_style = nodes(node);
+        let NodeStyle::Box(node_label) = node_style else {
+            // Ignore this node
+            continue;
+        };
 
         // Track the port counts for spacing
-        let ins = graph.num_inputs(n).max(1);
-        let outs = graph.num_outputs(n).max(1);
+        let ins = graph.num_inputs(node).max(1);
+        let outs = graph.num_outputs(node).max(1);
         let table_width = ins * outs;
 
-        let inputs_row = get_ports_row_dot(graph, n, Direction::Incoming, outs, &mut ports);
-        let outputs_row = get_ports_row_dot(graph, n, Direction::Outgoing, ins, &mut ports);
+        let inputs_row = get_ports_row_dot(graph, node, Direction::Incoming, outs, &mut ports);
+        let outputs_row = get_ports_row_dot(graph, node, Direction::Outgoing, ins, &mut ports);
 
-        let node_label = nodes(n);
         let label_row = format!(
             "<tr><td align=\"text\" border=\"0\" colspan=\"{table_width}\">{node_label}</td></tr>"
         );
 
-        let node_str = format!("{} [shape=plain label=<", n.index())
+        let node_str = format!("{} [shape=plain label=<", node.index())
             + "<table border=\"1\">"
             + &inputs_row
             + &label_row
@@ -134,9 +217,9 @@ fn node_and_edge_strings(
 
         // Connect the linked output ports
         graph
-            .outputs(n)
-            .enumerate()
-            .flat_map(|(offset, port)| get_edge_dot(graph, n, port, offset, &mut ports))
+            .outputs(node)
+            .flat_map(|port| graph.port_links(port))
+            .map(|(from, to)| get_edge_dot(graph, node, from, to, &mut edges))
             .for_each(|edge| {
                 dot_str.push_str(&edge);
             });
@@ -146,12 +229,12 @@ fn node_and_edge_strings(
 /// Outputs an html table row with the ports of a node.
 ///
 /// `num_others` is the number of ports in the other direction.
-fn get_ports_row_dot(
-    graph: &PortGraph,
+fn get_ports_row_dot<G: LinkView>(
+    graph: &G,
     node: NodeIndex,
     direction: Direction,
     num_others: usize,
-    mut ports: impl FnMut(PortIndex) -> (String, DotEdgeStyle),
+    mut ports: impl FnMut(PortIndex) -> PortStyle,
 ) -> String {
     if graph.num_ports(node, direction) == 0 {
         return String::new();
@@ -161,42 +244,51 @@ fn get_ports_row_dot(
         Direction::Outgoing => "out",
     };
 
+    let separator = |label: &str| if label.is_empty() { "" } else { ": " };
+
     let mut ports_row = "<tr>".to_string();
     for (offset, port) in graph.ports(node, direction).enumerate() {
-        let (port_label, _) = ports(port);
-        ports_row.push_str(&format!(
-            "<td port=\"{dir}{offset}\" align=\"text\" colspan=\"{num_others}\" cellpadding=\"1\">{offset}{separator}{port_label}</td>",
-            separator = if port_label.is_empty() { "" } else { ": " },
-        ));
+        let port_str = match ports(port) {
+            PortStyle::Hidden =>
+                format!("<td port=\"{dir}{offset}\" align=\"text\" colspan=\"0\"></td>"),
+            PortStyle::Text(label) =>
+                format!("<td port=\"{dir}{offset}\" align=\"text\" colspan=\"{num_others}\">{offset}{label}</td>"),
+            PortStyle::Box(label) => format!(
+                    "<td port=\"{dir}{offset}\" align=\"text\" colspan=\"{num_others}\" cellpadding=\"1\">{offset}{separator}{label}</td>",
+                    separator = separator(&label),
+                ),
+        };
+        ports_row.push_str(&port_str);
     }
     ports_row.push_str("</tr>");
     ports_row
 }
 
 /// If the port is linked, outputs the edge in dot format.
-fn get_edge_dot(
-    graph: &PortGraph,
+fn get_edge_dot<G: LinkView>(
+    graph: &G,
     from_node: NodeIndex,
-    from_port: PortIndex,
-    from_offset: usize,
-    mut ports: impl FnMut(PortIndex) -> (String, DotEdgeStyle),
-) -> Option<String> {
-    let to_port = graph.port_link(from_port)?;
-    let to_node = graph.port_node(to_port).expect("missing node");
-    let to_offset = graph.port_offset(to_port).expect("missing port").index();
-    let edge_style = ports(from_port).1.unwrap_or_default();
-    Some(format!(
-        "{}:out{} -> {}:in{} [style=\"{edge_style}\"]\n",
+    from: G::LinkEndpoint,
+    to: G::LinkEndpoint,
+    mut edges: impl FnMut(G::LinkEndpoint, G::LinkEndpoint) -> EdgeStyle,
+) -> String {
+    let from_offset = graph.port_offset(from).expect("missing port").index();
+    let to_node = graph.port_node(to).expect("missing node");
+    let to_offset = graph.port_offset(to).expect("missing port").index();
+    let edge_style = edges(from, to);
+    let edge_label = edge_style.as_str();
+    format!(
+        "{}:out{} -> {}:in{} [style=\"{edge_label}\"]\n",
         from_node.index(),
         from_offset,
         to_node.index(),
         to_offset,
-    ))
+    )
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{LinkView, PortView};
+    use crate::{PortGraph, PortView};
 
     use super::*;
 
@@ -239,6 +331,7 @@ mod tests {
             &hier,
             |_| Default::default(),
             |_| Default::default(),
+            |_, _| Default::default(),
         );
         let expected = r#"digraph {
 0 [shape=plain label=<<table border="1"><tr><td port="in0" align="text" colspan="2" cellpadding="1">0</td><td port="in1" align="text" colspan="2" cellpadding="1">1</td><td port="in2" align="text" colspan="2" cellpadding="1">2</td></tr><tr><td align="text" border="0" colspan="6"></td></tr><tr><td port="out0" align="text" colspan="3" cellpadding="1">0</td><td port="out1" align="text" colspan="3" cellpadding="1">1</td></tr></table>>]

1f4b022b73154f4d7afb96e98a0a780dd464e81b
Agustin Borgna <agustin.borgna@quantinuum.com>
2023-06-26 12:23:20 +0100

    Drop the substitute API

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -6,6 +6,5 @@ use criterion::criterion_main;
 criterion_main! {
     benchmarks::hierarchy::benches,
     benchmarks::portgraph::benches,
-    benchmarks::substitute::benches,
     benchmarks::toposort::benches,
 }

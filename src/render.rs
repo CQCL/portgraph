4e9ba8a4345b8f2bdbc79d5cac702afca1d4cb4f
Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>
2025-04-28 09:15:13 +0100

    feat: Node style attributes in render options (#199)
    Lets us specify the node colour / stroke when rendering mermaid or dot
    graphs.

--- src/render.rs
+++ src/render.rs
@@ -11,7 +11,53 @@ pub use mermaid::{MermaidFormat, MermaidFormatter};
 
 use self::mermaid::encode_label;
 
-/// Style of a rendered edge.
+/// Presentation attributes of a rendered element
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
+#[non_exhaustive]
+pub struct PresentationStyle {
+    /// Main color of the element.
+    ///
+    /// Encoded as an RGB hex string.
+    /// E.g. `#f00`, `#AE00FF`.
+    pub color: Option<String>,
+    /// Background fill color.
+    ///
+    /// Encoded as an RGB hex string.
+    /// E.g. `#f00`, `#AE00FF`.
+    pub fill: Option<String>,
+    /// Stroke color.
+    ///
+    /// Encoded as an RGB hex string.
+    /// E.g. `#ff0000`, `#AE00FF`.
+    pub stroke: Option<String>,
+    /// Stroke width.
+    ///
+    /// Encoded as pixels or a similar unit.
+    /// E.g. `1px`, `2pt`, `0.5em`.
+    pub stroke_width: Option<String>,
+}
+
+impl PresentationStyle {
+    /// Returns a new presentation style with no attributes set.
+    pub const fn new() -> Self {
+        Self {
+            color: None,
+            fill: None,
+            stroke: None,
+            stroke_width: None,
+        }
+    }
+
+    /// Returns `true` if the presentation style is empty.
+    pub const fn is_empty(&self) -> bool {
+        self.color.is_none()
+            && self.fill.is_none()
+            && self.stroke.is_none()
+            && self.stroke_width.is_none()
+    }
+}
+
+/// Style of a rendered node.
 ///
 /// Defaults to a box with no label.
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -20,19 +66,62 @@ pub enum NodeStyle {
     /// Ignore the node. No edges will be connected to it.
     Hidden,
     /// Draw a box with the label inside.
+    #[deprecated(since = "0.14.1", note = "Use `Boxed` instead")]
     Box(String),
+    /// Draw a box with label inside.
+    #[non_exhaustive]
+    Boxed {
+        /// Label of the node.
+        label: String,
+        /// Additional presentation attributes.
+        attrs: PresentationStyle,
+    },
 }
 
 impl NodeStyle {
     /// Show a node label with the default style.
+    #[deprecated(since = "0.14.1", note = "Use `boxed` instead")]
     pub fn new(label: impl ToString) -> Self {
-        Self::Box(label.to_string())
+        Self::Boxed {
+            label: label.to_string(),
+            attrs: Default::default(),
+        }
+    }
+
+    /// Show a node in a box.
+    ///
+    /// Additional presentation attributes can be set using [`NodeStyle::with_attrs`].
+    pub fn boxed(label: impl ToString) -> Self {
+        Self::Boxed {
+            label: label.to_string(),
+            attrs: Default::default(),
+        }
+    }
+
+    /// Set the presentation attributes of the node.
+    pub fn with_attrs(mut self, attrs: PresentationStyle) -> Self {
+        if let Self::Boxed { attrs: a, .. } = &mut self {
+            *a = attrs
+        }
+        self
+    }
+
+    /// Returns the presentation attributes of the node.
+    pub fn attrs(&self) -> &PresentationStyle {
+        static DEFAULT: PresentationStyle = PresentationStyle::new();
+        match self {
+            Self::Boxed { attrs, .. } => attrs,
+            _ => &DEFAULT,
+        }
     }
 }
 
 impl Default for NodeStyle {
     fn default() -> Self {
-        Self::Box(String::new())
+        Self::Boxed {
+            label: String::new(),
+            attrs: Default::default(),
+        }
     }
 }
 
@@ -144,6 +233,7 @@ impl EdgeStyle {
 }
 
 #[cfg(test)]
+#[allow(clippy::type_complexity)]
 mod test {
     use std::fmt::Display;
     use std::sync::OnceLock;
@@ -151,9 +241,9 @@ mod test {
     use rstest::{fixture, rstest};
 
     use crate::view::Region;
-    use crate::{Hierarchy, LinkMut, PortGraph, PortMut, PortView, Weights};
+    use crate::{Hierarchy, LinkMut, NodeIndex, PortGraph, PortMut, PortView, Weights};
 
-    use super::{DotFormat, MermaidFormat};
+    use super::{DotFormat, MermaidFormat, NodeStyle, PresentationStyle};
 
     /// A simple flat graph with some nodes and edges.
     #[fixture]
@@ -162,6 +252,7 @@ mod test {
         PortGraph,
         Option<Hierarchy>,
         Option<Weights<String, String>>,
+        Option<fn(NodeIndex) -> NodeStyle>,
     ) {
         let mut graph = PortGraph::new();
         let n1 = graph.add_node(3, 2);
@@ -169,7 +260,7 @@ mod test {
         let n3 = graph.add_node(1, 0);
         graph.link_nodes(n1, 0, n2, 0).unwrap();
         graph.link_nodes(n1, 1, n3, 0).unwrap();
-        ("flat", graph, None, None)
+        ("flat", graph, None, None, None)
     }
 
     #[fixture]
@@ -178,6 +269,7 @@ mod test {
         PortGraph,
         Option<Hierarchy>,
         Option<Weights<String, String>>,
+        Option<impl FnMut(NodeIndex) -> NodeStyle>,
     ) {
         let mut graph = PortGraph::new();
         let n1 = graph.add_node(3, 2);
@@ -189,7 +281,20 @@ mod test {
         hier.push_child(n2, n1).unwrap();
         hier.push_child(n3, n1).unwrap();
 
-        ("hierarchy", graph, Some(hier), None)
+        let node_style = move |n: NodeIndex| {
+            if n == n1 {
+                NodeStyle::boxed("root").with_attrs(PresentationStyle {
+                    color: Some("#f00".to_string()),
+                    fill: Some("#0f0".to_string()),
+                    stroke: Some("#00f".to_string()),
+                    stroke_width: Some("4px".to_string()),
+                })
+            } else {
+                NodeStyle::boxed(n.index())
+            }
+        };
+
+        ("hierarchy", graph, Some(hier), None, Some(node_style))
     }
 
     /// A hierarchical graph with edges between different regions.
@@ -199,6 +304,7 @@ mod test {
         PortGraph,
         Option<Hierarchy>,
         Option<Weights<String, String>>,
+        Option<fn(NodeIndex) -> NodeStyle>,
     ) {
         let mut graph = PortGraph::new();
         let n1 = graph.add_node(3, 2);
@@ -215,7 +321,7 @@ mod test {
         hier.push_child(n4, n2).unwrap();
         hier.push_child(n5, n3).unwrap();
 
-        ("hierarchy_interregional", graph, Some(hier), None)
+        ("hierarchy_interregional", graph, Some(hier), None, None)
     }
 
     #[fixture]
@@ -224,6 +330,7 @@ mod test {
         PortGraph,
         Option<Hierarchy>,
         Option<Weights<String, String>>,
+        Option<fn(NodeIndex) -> NodeStyle>,
     ) {
         let mut graph = PortGraph::new();
         let n1 = graph.add_node(0, 2);
@@ -246,18 +353,18 @@ mod test {
         weights[p20] = "in 0".to_string();
         weights[p30] = "in 0".to_string();
 
-        ("weighted", graph, None, Some(weights))
+        ("weighted", graph, None, Some(weights), None)
     }
 
     #[allow(clippy::type_complexity)]
-    type RegionViewResult = (
+    #[fixture]
+    fn region_view() -> (
         &'static str,
         Region<'static, PortGraph>,
         Option<Hierarchy>,
         Option<Weights<String, String>>,
-    );
-    #[fixture]
-    fn region_view() -> RegionViewResult {
+        Option<fn(NodeIndex) -> NodeStyle>,
+    ) {
         let mut graph = PortGraph::new();
         let other = graph.add_node(42, 0);
         let root = graph.add_node(1, 0);
@@ -279,7 +386,7 @@ mod test {
 
         let region = Region::new(graph, hierarchy, root);
 
-        ("region_view", region, Some(hierarchy.clone()), None)
+        ("region_view", region, Some(hierarchy.clone()), None, None)
     }
 
     #[rstest]
@@ -295,19 +402,25 @@ mod test {
             impl MermaidFormat,
             Option<Hierarchy>,
             Option<Weights<WN, WP>>,
+            Option<impl FnMut(NodeIndex) -> NodeStyle>,
         ),
     ) {
-        let (name, graph, hierarchy, weights) = graph_elems;
-        let mermaid = match (hierarchy, weights) {
-            (Some(h), Some(w)) => graph
-                .mermaid_format()
-                .with_hierarchy(&h)
-                .with_weights(&w)
-                .finish(),
-            (Some(h), None) => graph.mermaid_format().with_hierarchy(&h).finish(),
-            (None, Some(w)) => graph.mermaid_format().with_weights(&w).finish(),
-            (None, None) => graph.mermaid_string(),
+        let (name, graph, hierarchy, weights, node_style) = graph_elems;
+
+        let fmt = graph.mermaid_format();
+        let fmt = match &hierarchy {
+            Some(h) => fmt.with_hierarchy(h),
+            None => fmt,
         };
+        let fmt = match node_style {
+            Some(node_style) => fmt.with_node_style(node_style),
+            None => fmt,
+        };
+        let fmt = match &weights {
+            Some(w) => fmt.with_weights(w),
+            None => fmt,
+        };
+        let mermaid = fmt.finish();
 
         let name = format!("{}__mermaid", name);
         insta::assert_snapshot!(name, mermaid);
@@ -325,19 +438,25 @@ mod test {
             impl DotFormat,
             Option<Hierarchy>,
             Option<Weights<WN, WP>>,
+            Option<impl FnMut(NodeIndex) -> NodeStyle>,
         ),
     ) {
-        let (name, graph, hierarchy, weights) = graph_elems;
-        let dot = match (hierarchy, weights) {
-            (Some(h), Some(w)) => graph
-                .dot_format()
-                .with_hierarchy(&h)
-                .with_weights(&w)
-                .finish(),
-            (Some(h), None) => graph.dot_format().with_hierarchy(&h).finish(),
-            (None, Some(w)) => graph.dot_format().with_weights(&w).finish(),
-            (None, None) => graph.dot_string(),
+        let (name, graph, hierarchy, weights, node_style) = graph_elems;
+
+        let fmt = graph.dot_format();
+        let fmt = match &hierarchy {
+            Some(h) => fmt.with_hierarchy(h),
+            None => fmt,
         };
+        let fmt = match node_style {
+            Some(node_style) => fmt.with_node_style(node_style),
+            None => fmt,
+        };
+        let fmt = match &weights {
+            Some(w) => fmt.with_weights(w),
+            None => fmt,
+        };
+        let dot = fmt.finish();
 
         let name = format!("{}__dot", name);
         insta::assert_snapshot!(name, dot);

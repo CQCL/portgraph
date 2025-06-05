b363635fb30543e019e27284b278159454a1db79
Duong H. D. <79622476+gluonhiggs@users.noreply.github.com>
2025-06-05 20:10:49 +0700

    feat: added DynamicTopoConvexChecker (#212)
    ## Description
    
    I have added the logic for `DynamicTopoConvexCheck` in the
    `convexity.rs` as requested
    [hugr](https://github.com/CQCL/hugr/pull/2276) to resolve this
    [issue](https://github.com/CQCL/hugr/issues/2240)
    
    ---------
    
    Co-authored-by: Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -2,12 +2,14 @@
 
 mod convex;
 mod dominators;
+mod dynamic;
 mod lca;
 mod post_order;
 mod toposort;
 
 pub use convex::{ConvexChecker, TopoConvexChecker};
 pub use dominators::{dominators, dominators_filtered, DominatorTree};
+pub use dynamic::DynamicTopoConvexChecker;
 pub use lca::{lca, LCA};
 pub use post_order::{postorder, postorder_filtered, PostOrder};
 pub use toposort::{toposort, toposort_filtered, TopoSort};
